667997cf738e31ff71a485151618d6517785e4b5
Himanshu Yadav <106692689+github-himanshu-yadav@users.noreply.github.com>
2022-07-30 18:09:57 +0530

    Add files via upload

---
+++ Electronics.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Electronics.css'
+import Product from './Product'
+function Electronics({title}) {
+  return (
+    <div className='electronics'>
+      <div className='electronics-title'>{title}</div>
+        <div className='electronics-content'>
+      
+          <Product
+          desc='SONY Alpha 7M3K Mirrorless Camera Body with 28 - 70 mm Zoom Lens  (Black)'
+          image='https://rukminim1.flixcart.com/image/832/832/kw9krrk0/dslr-camera/q/l/w/-original-imag8z5wwaqtq9bz.jpeg?q=70'
+          />
+          <Product
+          desc='D-Link DIR-650IN 300 Mbps Wireless Router  (Black, Single Band)'
+          image='https://rukminim1.flixcart.com/image/832/832/kbnz8nk0/router/x/y/k/d-link-dir-650in-original-imafsysqj3f3tec5.jpeg?q=70'
+          />
+          <Product
+          desc='SAMSUNG 24 inch Full HD LED Backlit IPS Panel with 3-Sided Borderless Display, Game & Free Sync Mode, Eye Saver Mode'
+          image='https://rukminim1.flixcart.com/image/832/832/l5ld8y80/monitor/l/k/s/-original-imagg897ufhyvwqq.jpeg?q=70'
+          />
+           <Product
+          desc='SONY Alpha 7M3K Mirrorless Camera Body with 28 - 70 mm Zoom Lens  (Black)'
+          image='https://rukminim1.flixcart.com/image/832/832/kw9krrk0/dslr-camera/q/l/w/-original-imag8z5wwaqtq9bz.jpeg?q=70'
+          />
+          <Product
+          desc='D-Link DIR-650IN 300 Mbps Wireless Router  (Black, Single Band)'
+          image='https://rukminim1.flixcart.com/image/832/832/kbnz8nk0/router/x/y/k/d-link-dir-650in-original-imafsysqj3f3tec5.jpeg?q=70'
+          />
+          <Product
+          desc='SAMSUNG 24 inch Full HD LED Backlit IPS Panel with 3-Sided Borderless Display, Game & Free Sync Mode, Eye Saver Mode'
+          image='https://rukminim1.flixcart.com/image/832/832/l5ld8y80/monitor/l/k/s/-original-imagg897ufhyvwqq.jpeg?q=70'
+          />
+           <Product
+          desc='SONY Alpha 7M3K Mirrorless Camera Body with 28 - 70 mm Zoom Lens  (Black)'
+          image='https://rukminim1.flixcart.com/image/832/832/kw9krrk0/dslr-camera/q/l/w/-original-imag8z5wwaqtq9bz.jpeg?q=70'
+          />
+          <Product
+          desc='D-Link DIR-650IN 300 Mbps Wireless Router  (Black, Single Band)'
+          image='https://rukminim1.flixcart.com/image/832/832/kbnz8nk0/router/x/y/k/d-link-dir-650in-original-imafsysqj3f3tec5.jpeg?q=70'
+          />
+          <Product
+          desc='SAMSUNG 24 inch Full HD LED Backlit IPS Panel with 3-Sided Borderless Display, Game & Free Sync Mode, Eye Saver Mode'
+          image='https://rukminim1.flixcart.com/image/832/832/l5ld8y80/monitor/l/k/s/-original-imagg897ufhyvwqq.jpeg?q=70'
+          />
+
+        </div>
+    </div>
+  )
+}
+
+export default Electronics
